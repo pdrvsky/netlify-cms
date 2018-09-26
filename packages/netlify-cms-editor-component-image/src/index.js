1c79ab14132789ddab20b41d0567ae63d28e5bbf
Piotr Rasztabiga <prtestpl@gmail.com>
2018-09-26 12:39:42 +0200

    Implemented image title attribute in editor component.

--- packages/netlify-cms-editor-component-image/src/index.js
+++ packages/netlify-cms-editor-component-image/src/index.js
@@ -7,11 +7,12 @@ const image = {
     match && {
       image: match[2],
       alt: match[1],
+      title: match[3],
     },
-  toBlock: data => `![${data.alt || ''}](${data.image || ''})`,
+  toBlock: data => `![${data.alt || ''}](${data.image || ''} "${data.title}")`,
   // eslint-disable-next-line react/display-name
-  toPreview: (data, getAsset) => <img src={getAsset(data.image) || ''} alt={data.alt || ''} />,
-  pattern: /^!\[(.*)\]\((.*)\)$/,
+  toPreview: (data, getAsset) => <img src={getAsset(data.image) || ''} alt={data.alt || ''} title={data.title || ''} />,
+  pattern: /^!\[(.*)\]\((.*)[ ]{1}"(.*)"\)$/,
   fields: [
     {
       label: 'Image',
@@ -21,6 +22,10 @@ const image = {
     {
       label: 'Alt Text',
       name: 'alt',
+    },
+    {
+      label: 'Title Text',
+      name: 'title',
     },
   ],
 };
